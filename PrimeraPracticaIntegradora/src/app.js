df03c2887e03c476601b4dbb1f20668817aa3aeb
Yasmin Afsharinejad <iasaminblack@gmail.com>
2023-05-29 21:19:27 -0300

    Desafíos

---
+++ PrimeraPracticaIntegradora/src/app.js
@@ -0,0 +1,20 @@
+
+import express from "express";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import "./db/db.js";
+import productsRouter from "./routes/products.router.js";
+//import cartsRouter from "./routes/carts.router.js";
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(errorHandler);
+
+app.use("/api/products", productsRouter);
+//app.use("/api/carts", cartsRouter);
+
+app.listen(PORT, () => {
+  console.log("🚀 Server listening on port 8080");
+});
