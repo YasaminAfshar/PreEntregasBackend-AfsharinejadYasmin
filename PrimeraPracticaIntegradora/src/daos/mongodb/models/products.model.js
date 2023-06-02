0d909b60b220502ac2c9f3a85b7f6e98f028579a
Yasmin Afsharinejad <iasaminblack@gmail.com>
2023-06-02 18:35:21 -0300

    Desafios

--- PrimeraPracticaIntegradora/src/daos/mongodb/models/products.model.js
+++ PrimeraPracticaIntegradora/src/daos/mongodb/models/products.model.js
@@ -4,14 +4,14 @@ import mongoose from "mongoose";
 const productsCollection = "products";
 
 const productsSchema = new mongoose.Schema({
-  title: { type: String, required: true, max:250 },
-  description: { type: String, required: true, max:500 },
+  title: { type: String, required: true, maxLength: 250 },
+  description: { type: String, required: true, maxLength: 500 },
   price: { type: Number, required: true },
-  code: { type: String, required: true, unique: true, max:6 },
-  category: {type: String, required: true},
-  stock: { type: Number, required: true },
-  thumbnails: { type: String, required: true},
-  status: {type: Boolean, default: true},
+  code: { type: String, required: true, unique: true, maxLength: 6 },
+  category: { type: String, required: true },
+  stock: { type: Number, required: true, max: 100 },
+  thumbnails: { type: String, required: true },
+  status: { type: Boolean, default: true },
 });
 
 export const ProductsModel = mongoose.model(productsCollection, productsSchema);
