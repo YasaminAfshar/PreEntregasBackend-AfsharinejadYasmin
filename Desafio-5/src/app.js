c575d1ce935c47332b8b81edb447d1c1e6f33f44
Yasmin Afsharinejad <iasaminblack@gmail.com>
2023-06-15 19:16:23 -0300

    Desafios

---
+++ Desafio-5/src/app.js
@@ -0,0 +1,65 @@
+
+import express from "express";
+import { __dirname } from "./path.js";
+import handlebars from "express-handlebars";
+import { Server } from "socket.io";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import "./db/db.js";
+import productsRouter from "./routes/products.router.js";
+import cartsRouter from "./routes/carts.router.js";
+import messagesRouter from "./routes/messages.router.js";
+
+import MessagesDaoMongo from "./daos/mongodb/messages.dao.js";
+const messagesManager = new MessagesDaoMongo();
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use(errorHandler);
+
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/views");
+
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
+
+app.use("/messages", messagesRouter);
+
+
+const httpServer = app.listen(PORT, () => {
+  console.log("🚀 Server listening on port 8080");
+});
+
+const socketServer = new Server(httpServer);
+
+socketServer.on("connection", async (socket) => {
+  console.log("¡🟢 New connection!", socket.id);
+
+  socketServer.emit("messages", await messagesManager.getAllMessages());
+
+  socket.on("disconnect", () => {
+    console.log("¡🔴 User disconnect!");
+  });
+
+  socket.on("newUser", (userName) => {
+    console.log(`${userName} is logged in`);
+  });
+
+  socket.on("chat:message", async ({ userName, message }) => {
+    await messagesManager.createMessage(userName, message);
+    socketServer.emit("messages", await messagesManager.getAllMessages());
+  });
+
+  socket.on("newUser", (userName) => {
+    socket.broadcast.emit("newUser", userName);
+  });
+
+  socket.on("chat:typing", (data) => {
+    socket.broadcast.emit("chat:typing", data);
+  });
+
+});
---
+++ Desafio-5/src/services/carts.services.js
@@ -0,0 +1,77 @@
+import CartsDaoMongo from "../daos/mongodb/carts.dao.js";
+const cartsManager = new CartsDaoMongo();
+
+//import CartDaoFs from "../daos/filesystem/carts.dao.js";
+//const cartsManager = new CartDaoFs();
+
+export const getCartsAllService = async () => {
+  try {
+    const docs = await cartsManager.getAllCart();
+    return docs;
+  } catch (error) {
+        console.log(error);
+  }
+};
+
+export const getCartByIdService = async (cid) => {
+  try {
+    const doc = await cartsManager.getCartById(cid);
+    //const doc = await cartsManager.getCartById(Number(cid));
+    if (!doc)
+      return `The cart you are searching width ID ${cid} could not be found!`;
+    else return doc;
+  } catch (error) {
+        console.log(error);
+  }
+};
+
+export const createCartService = async () =>{
+    try {
+        const doc = await cartsManager.createCart();
+        return doc;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const addProductToCartService = async (cid, pid) =>{
+    try {
+        const doc = await cartsManager.addProductToCart(cid, pid);
+        //const doc = await cartsManager.addProductToCart(Number(cid), Number(pid));
+        return doc;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const updateProductQuantityService = async(cid, pid, quantity) => {
+  try {
+      const doc = await cartsManager.updateProductQuantity(cid, pid, quantity);
+      return doc;
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+
+export const deleteProductToCartService = async (cid) => {
+  try {
+    const doc = await cartsManager.deleteProductToCart(cid);
+    //const doc = await cartsManager.deleteProductToCart(Number(cid));
+    return doc;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+export const deleteProductFromCartService = async (cid, pid) => {
+  try {
+    const doc = await cartsManager.deleteProductFromCart(cid, pid);
+    //const doc = await cartsManager.deleteProductFromCart(Number(cid), Number(pid));
+    return doc;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
